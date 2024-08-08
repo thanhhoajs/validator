07b86290691cc3d420333c5149080c1a370202ee
Nguyễn Như Khánh <105545726+nguyennhukhanh@users.noreply.github.com>
2024-08-08 20:30:36 +0700

    chore: format code (#2)
    * feat: add some methods
    
    * feat: update code

---
+++ src/shared/interfaces/validator.interface.ts
@@ -0,0 +1,29 @@
+import { type IValidationError, FieldValidator } from '@thanhhoajs/validator';
+
+export interface IValidator {
+  /**
+   * Returns a FieldValidator instance for the given field name.
+   *
+   * @param {string} name - The name of the field.
+   * @return {FieldValidator} A FieldValidator instance for the given field name.
+   */
+  field(name: string): FieldValidator;
+
+  /**
+   * Validates the data based on the defined rules and returns any validation errors.
+   *
+   * @param {Record<string, any>} data - The data to be validated.
+   * @return {IValidationError[]} An array of validation errors, if any.
+   */
+  validate(data: Record<string, any>): IValidationError[];
+
+  /**
+   * Configures the validations for each field using the provided `validations` object.
+   *
+   * @param {Record<string, (fieldValidator: FieldValidator) => void>} validations - An object containing the field names as keys and the corresponding validation functions as values.
+   * @return {void} This function does not return a value.
+   */
+  configure(
+    validations: Record<string, (fieldValidator: FieldValidator) => void>,
+  ): void;
+}
